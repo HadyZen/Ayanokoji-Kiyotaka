274e37ef3a585fc5a4dc8fcb854a21e838acc8f2
Hady Zen'in条 <149669803+HadyZen@users.noreply.github.com>
2025-02-06 18:43:47 +0700

    Hady Zen

---
+++ perintah/status.js
@@ -0,0 +1,14 @@
+module.exports = {
+  hady: { 
+    nama: "status", 
+    penulis: "Hady Zen", 
+    kuldown: 10,
+    peran: 0,
+    tutor: ""
+  }, 
+  
+  Ayanokoji: async function ({ api, event, getData }) {
+    const { nama, level, exp, yen } = getData(event.senderID);
+    api.sendMessage(`🜲 𝗦𝘁𝗮𝘁𝘂𝘀 \nNama: ${nama}\nId: ${event.senderID}\nYen: ${yen}\nLevel: ${level}`, event.threadID, event.messageID);
+  }
+};
