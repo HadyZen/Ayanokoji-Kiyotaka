89d55469ca4152eea69ae58754e9e04e8aceb8aa
Hady Zen'in条 <149669803+HadyZen@users.noreply.github.com>
2025-02-22 18:52:13 +0700

    Hady Zen

--- perintah/daily.js
+++ perintah/daily.js
@@ -9,11 +9,13 @@ module.exports = {
   
   Ayanokoji: async function ({ api, event, getData, setUser }) {
   const { yen, exp, daily } = getData(event.senderID);
+    
     if (daily == null || daily !== global.Ayanokoji.tanggal) { 
-  setUser(event.senderID, 'yen', yen + 2);
+  const duit = Math.floor(Math.random() * 10);
+  setUser(event.senderID, 'yen', yen + duit);
   setUser(event.senderID, 'exp', exp + 10);
   setUser(event.senderID, 'daily', global.Ayanokoji.tanggal);
-    api.sendMessage("Kamu berhasil mengklaim 2 yen dan 10 exp", event.threadID, event.messageID);
+    api.sendMessage(`Kamu berhasil mengklaim ${duit} yen dan 10 exp`, event.threadID, event.messageID);
     } else {
     api.sendMessage('Kamu sudah mengklaim hadiah harian.', event.threadID, event.messageID);
   }
