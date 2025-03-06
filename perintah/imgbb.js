f744c7036444219c2143a33469bcd26d594b2169
Hady Zen'in条 <149669803+HadyZen@users.noreply.github.com>
2025-03-06 17:58:19 +0700

    Hady Zen

---
+++ perintah/imgbb.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const from = require('form-data');
+
+module.exports = {
+  hady: {
+    nama: "imgbb",
+    penulis: "Hady Zen",
+    kuldown: 10,
+    peran: 0,
+    tutor: "<reply foto>"
+  },
+
+  Ayanokoji: async function ({ api, event, bhs }) {
+    const itsuki = event.messageReply?.attachments[0]?.url;
+    if (!itsuki) {
+      return api.sendMessage('Kamu harus memreply ke sebuah gambar.', event.threadID, event.messageID);
+    }
+
+    try {
+      const miku = await axios.get(itsuki, {
+        responseType: 'arraybuffer'
+      });
+      const formData = new from();
+      formData.append('image', Buffer.from(miku.data, 'binary'), {
+        filename: 'hady-zen.png'
+      });
+      const nino = await axios.post('https://api.imgbb.com/1/upload', formData, {
+        params: {
+          key: "1b4d99fa0c3195efe42ceb62670f2a25"
+        }
+      });
+      await api.sendMessage(nino.data.data.url, event.threadID, event.messageID);
+    } catch (hadi) {
+      api.sendMessage('Error: ' + hadi, event.threadID, event.messageID);
+    }
+  },
+};
